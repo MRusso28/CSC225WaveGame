cb5f931ffb50379c41c8a95a49a688ab1f060839
Adam Curley <adamrcurley@adams-air>
2019-10-10 18:41:30 -0400

    changed variable names

--- src/mainGame/Leaderboard.java
+++ src/mainGame/Leaderboard.java
@@ -58,11 +58,11 @@ public class Leaderboard {
 	public class NumericalStringComparator implements Comparator<String> {
 	    @Override
 	    public int compare (String s1, String s2) {
-	        int i1 = Integer.parseInt(s1.split(" ")[0]);
-	        int i2 = Integer.parseInt(s2.split(" ")[0]);
-	        int cmp = Integer.compare(i1, i2);
-	        if (cmp != 0) {
-	            return cmp;
+	        int int1 = Integer.parseInt(s1.split(" ")[0]);
+	        int int2 = Integer.parseInt(s2.split(" ")[0]);
+	        int comp = Integer.compare(int1, int2);
+	        if (comp != 0) {
+	            return comp;
 	        }
 	        return s1.compareTo(s2);
 	    }
